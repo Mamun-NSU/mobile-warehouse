09e4e948bb784d37240611d99979e0f0c5c69cac
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-03-01 10:57:22 +0600

    show phone data browser successfully

--- javascript/app.js
+++ javascript/app.js
@@ -8,5 +8,27 @@ const searchPhone = () => {
     console.log(url);
     fetch(url)
         .then(response => response.json())
-        .then(data => console.log(data.data));
+        .then(data => DisplaySearchResult(data.data));
+}
+
+const DisplaySearchResult = (phones) => {
+    
+    console.log(phones);
+    const searchResult = document.getElementById('search-result');
+
+    phones.forEach(phone => {
+        // console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">${phone.brand}</p>
+            </div>
+        </div>
+        `;
+        searchResult.appendChild(div);
+    });
 }
